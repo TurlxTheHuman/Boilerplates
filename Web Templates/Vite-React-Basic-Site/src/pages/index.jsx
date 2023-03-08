0f960c67dcb8d3fcb72e090798e7cc864686f0f5
Turlx <contact@turlxthehuman.com>
2023-03-07 21:27:00 -0800

    VITEJS-Boilerplate

---
+++ Web Templates/Vite-React-Basic-Site/src/pages/index.jsx
@@ -0,0 +1,19 @@
+import { useState, useCallback } from 'react';
+
+function Main() {
+  return (
+    <body>
+      <section id='hero'>
+        <p>Hero Content</p>
+      </section>
+
+      <section className='quad_grid'>
+        <p>Other Stuff</p>
+      </section>
+
+
+    </body>
+  )
+}
+
+export default Main
